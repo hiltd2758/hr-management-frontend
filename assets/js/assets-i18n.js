2e0be4ce03ddfe7207ae93e3905a2d169782a821
tinhonline262 <tinhonline262@gmail.com>
2025-09-13 08:13:52 +0700

    update i18n

---
+++ assets/js/assets-i18n.js
@@ -0,0 +1,17 @@
+// JavaScript function to change language
+function changeLanguage(lang) {
+    if (typeof I18n !== 'undefined') {
+        const i18n = new I18n();
+        i18n.changeLanguage(lang);
+    }
+}
+
+// Update the i18n initialization script in assets.html
+document.addEventListener('DOMContentLoaded', function() {
+    const i18n = new I18n('assets');
+    
+    // Make changeLanguage function available globally
+    window.changeLanguage = function(lang) {
+        i18n.changeLanguage(lang);
+    };
+});
